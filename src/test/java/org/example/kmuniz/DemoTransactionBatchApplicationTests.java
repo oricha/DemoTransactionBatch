f0f4bdf19521d3ff803cd1f08cae569ad72f50ab
Karel <oricha@gmail.com>
2025-01-23 00:03:09 +0100

    .gitignore and Tests

--- src/test/java/org/example/kmuniz/DemoTransactionBatchApplicationTests.java
+++ src/test/java/org/example/kmuniz/DemoTransactionBatchApplicationTests.java
@@ -6,7 +6,6 @@ import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = DemoTransactionBatchApplication.class)
 public class DemoTransactionBatchApplicationTests {
 
 	@Test
